da45fa4a8ac94965d4b8555a200d06605b5dda84
Sean Grant <seangrant118@gmail.com>
2020-03-03 17:57:35 -0500

    bcrypt compare function

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,15 @@ userSchema.pre("save", function(next) {
   });
 });
 
+userSchema.methods.comparePassword = function(candidate, callback) {
+  bcrpyt.compare(candidate, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
+};
+
 //Create the model class
 const ModelClass = mongoose.model("user", userSchema);
 
