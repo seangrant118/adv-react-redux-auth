a06d229901de56c51c610e69681455727718914a
Sean Grant <seangrant118@gmail.com>
2020-03-03 16:05:47 -0500

    installing passport

---
+++ server/services/passport.js
@@ -0,0 +1,5 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const User = require("../models/user");
+const config = require("../config");
