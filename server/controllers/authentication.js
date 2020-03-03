82d83586c42b171f9d38cb046ed225d99632ca7b
Sean Grant <seangrant118@gmail.com>
2020-03-03 15:39:36 -0500

    encryption with bcrypt

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,13 +4,19 @@ exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password" });
+  }
+
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) {
       return next(err);
     }
 
     if (existingUser) {
-      return res.status(422).send({ error: "email is in use" });
+      return res.status(422).send({ error: "Email is in use" });
     }
 
     const user = new User({
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,31 @@
 const mongoose = require("mongoose");
+const bcrpyt = require("bcrypt-nodejs");
 const Schema = mongoose.Schema;
 
 //Define model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+//On Save Hook, encrypt password
+userSchema.pre("save", function(next) {
+  const user = this;
+
+  bcrpyt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrpyt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 //Create the model class
