7bfa7fe3b9bd389e51ca052b13031bf1c0b6b62c
Sean Grant <seangrant118@gmail.com>
2020-03-03 15:46:12 -0500

    salting a password

--- server/models/user.js
+++ server/models/user.js
@@ -10,18 +10,22 @@ const userSchema = new Schema({
 
 //On Save Hook, encrypt password
 userSchema.pre("save", function(next) {
+  // get access to user model
   const user = this;
 
+  // generate a salt then run callback
   bcrpyt.genSalt(10, function(err, salt) {
     if (err) {
       return next(err);
     }
 
+    // hash (encrypt) our password using salt
     bcrpyt.hash(user.password, salt, null, function(err, hash) {
       if (err) {
         return next(err);
       }
 
+      //overwrite plain text password with encrypted password
       user.password = hash;
       next();
     });
