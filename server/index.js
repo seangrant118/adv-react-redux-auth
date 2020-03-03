0543d22b69a0924d3963c36622477ef3b30a39e1
Sean Grant <seangrant118@gmail.com>
2020-03-03 13:48:00 -0500

    mongo

--- server/index.js
+++ server/index.js
@@ -4,6 +4,20 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
 const router = require("./router");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3090;
+
+//DB setup
+mongoose.connect("mongodb://localhost/auth", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", function() {
+  console.log("connected to db");
+});
 
 //App Setup
 app.use(morgan("combined"));
@@ -11,7 +25,5 @@ app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
 //Server Setup
-const port = process.env.PORT || 3090;
-const server = http.createServer(app);
-server.listen(port);
+app.listen(port);
 console.log("Server listening on:", port);
