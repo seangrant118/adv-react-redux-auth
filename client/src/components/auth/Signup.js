0fce61ab89ebe7c3eff42101831bd5127900c941
Sean Grant <seangrant118@gmail.com>
2020-03-04 21:36:22 -0500

    code cleanup with compose

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
+import { compose } from "redux";
 import { connect } from "react-redux";
 import * as actions from "../../actions";
 
 class Signup extends React.Component {
   onSubmit = formProps => {
-    console.log(formProps);
+    this.props.signup(formProps);
   };
 
   render() {
@@ -36,4 +37,7 @@ class Signup extends React.Component {
   }
 }
 
-export default reduxForm({ form: "signup" })(Signup);
+export default compose(
+  connect(null, actions),
+  reduxForm({ form: "signup" })
+)(Signup);
