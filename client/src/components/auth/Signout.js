dcc7b057a795e79c3c9984ab35ec8c0e713aef95
Sean Grant <seangrant118@gmail.com>
2020-03-05 12:11:40 -0500

    signing out a user

---
+++ client/src/components/auth/Signout.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Signout extends React.Component {
+  render() {
+    return <div>Sorry to see you go!</div>;
+  }
+}
+
+export default Signout;
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
+import Signout from "./components/auth/Signout";
 import reducers from "./reducers";
 import Feature from "./components/Feature";
 
@@ -24,6 +25,7 @@ ReactDom.render(
       <App>
         <Route path="/" component={Welcome} exact={true} />
         <Route path="/signup" component={Signup} />
+        <Route path="/signout" component={Signout} />
         <Route path="/feature" component={Feature} />
       </App>
     </BrowserRouter>
