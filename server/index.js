7d412516da17a7b5cabbdf0fc631a64534b06860
Sean Grant <seangrant118@gmail.com>
2020-03-04 21:55:00 -0500

    solution to CORS error

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const app = express();
 const router = require("./router");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const port = process.env.PORT || 3090;
 
 //DB setup
@@ -21,6 +22,7 @@ connection.on("connected", function() {
 
 //App Setup
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
